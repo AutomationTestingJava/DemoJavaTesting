cd5c8d2ddc2a0e52d7cb35a7b27cda2fd277bff9
Automation <aakashkangule@gmail.com>
2023-08-02 15:45:53 +0530

    first Commit

---
+++ src/main/java/com/demos/Calculator.java
@@ -0,0 +1,17 @@
+package com.demos;
+
+public class Calculator {
+public static int Add(int i,int j)
+{
+	return i+j;
+}
+public static int Multiply(int i,int j)
+{
+	return i*j;
+}
+public static int Divide(int i,int j)
+{
+	return i/j;
+}
+
+}
---
+++ src/test/java/Test/CalculatorTest.java
@@ -0,0 +1,9 @@
+package Test;
+
+import com.demos.Calculator;
+
+public class CalculatorTest {
+Calculator cal=new Calculator();
+
+
+}
